46b6d39c767b6c8d5c661c36c6693035ad7cc9cb
Martin Asenov <m.asenov5752@gmail.com>
2019-06-28 16:01:14 +0300

    Task finished

---
+++ Intern.MM/Dto/PlayerDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intern.MM.Dto
+{
+    public class PlayerDto
+    {
+        public string Name { get; set; }
+
+        public string PlayingSince { get; set; }
+
+        public string Position { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ Intern.MM/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using Intern.MM.Dto;
+using ServiceStack;
+
+namespace Intern.MM
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Test Input
+            CsvFilteredPlayers(@"D:\Programming\Software University\Intern.MM\Intern.MM\Dataset\NBA-Players.json", 20, 30, @"D:\Programming\Software University\Intern.MM\Intern.MM\OutputCsv\Test.csv");
+        }
+
+        public static void CsvFilteredPlayers(string path, int maximumYearsOfPlay, int minimumPlayerRating, string pathToCsvFileToGenerate)
+        {
+            var filteredPlayers = JsonConvert.DeserializeObject<PlayerDto[]>(File.ReadAllText(path))
+                .Where(p => p.Rating >= minimumPlayerRating && (DateTime.Now.Year - int.Parse(p.PlayingSince)) <= maximumYearsOfPlay).Select(fp => new
+                {
+                    fp.Name,
+                    fp.Rating
+                })
+                .OrderByDescending(fp => fp.Rating)
+                .ToCsv()
+                .TrimEnd();
+
+            File.WriteAllText(pathToCsvFileToGenerate, filteredPlayers);
+        }
+    }
+}
